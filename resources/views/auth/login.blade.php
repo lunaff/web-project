911e2b6ad3a07e24cd8c70baf7ac454d9eeea75d
nekichen <naunaulia@gmail.com>
2025-02-15 18:33:37 +0700

    fix : comment name & level on login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,13 +57,13 @@
                                         </div>
 
                                         <!-- Username Field -->
-                                        <div class="mb-3">
+                                        <!-- <div class="mb-3">
                                             <label class="form-label" for="name">Name</label>
                                             <div class="position-relative input-custom-icon">
                                                 <input type="text" class="form-control" id="name" name="name" placeholder="Enter name">
                                                 <span class="bx bx-user"></span>
                                             </div>
-                                        </div>
+                                        </div> -->
                                     
                                         <!-- Password Field -->
                                         <div class="mb-3">
@@ -81,7 +81,7 @@
                                         </div>
 
                                         <!-- Level Field -->
-                                        <div class="mb-3">
+                                        <!-- <div class="mb-3">
                                             <label class="form-label" for="level">Level</label>
                                             <select class="form-control" id="level" name="level">
                                                 <option value="admin">Admin</option>
@@ -90,7 +90,7 @@
                                                 <option value="bk">BK</option>
                                                 <option value="kesiswaan">Kesiswaan</option>
                                             </select>
-                                        </div>
+                                        </div> -->
                                     
                                         <!-- Remember Me -->
                                         <div class="form-check py-1">
